f8fb7062ad06ae57670bdfdfe282b80117fecf6d
Haris Wahidin <hrswhdn@gmail.com>
2025-03-18 10:33:49 +0700

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  bool isUpdating = false;
 
   void _incrementCounter() {
     setState(() {
